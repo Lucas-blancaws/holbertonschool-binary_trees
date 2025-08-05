86be477af01dcca68d94c35aa04d94df0df8285b
salagreg <greg.sala@icloud.com>
2025-08-05 22:39:59 +0200

    Compter nombre noeuds avec enfant

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_nodes - Compte le nombre de nœuds avec au moins un enfant
+ * dans un arbre binaire
+ * @tree: Pointeur vers la racine de l'arbre binaire
+ * Return: Nombre nœuds avec au moins un enfant, 0 si l'arbre est NULL
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+  /* Vérifie si l'arbre est NULL */
+  if (tree == NULL)
+  {
+    return (0);
+  }
+
+  /* Si le nœud a au moins un enfant, ajoute 1 et compte récursivement */
+  if (tree->left != NULL || tree->right != NULL)
+  {
+    return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+  }
+else
+{
+    /* Si le nœud n'a pas d'enfant, continue le parcours récursif */
+    return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+}
+}
