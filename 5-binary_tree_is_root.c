96455f8427c03ce2b0f0668279be0dc7eb3c98f4
salagreg <greg.sala@icloud.com>
2025-08-05 10:12:22 +0200

    Verifier racine noeud

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_root - Vérifie si un nœud est la racine d'un arbre binaire
+ * @node: Pointeur vers le nœud à vérifier
+ * Return: 1 si le nœud est la racine, 0 sinon ou si le nœud est NULL
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+  /* Vérifie si le nœud est NULL */
+  if (node == NULL)
+  {
+    return (0);
+  }
+
+  /* Vérifie si le nœud n'a pas de parent */
+  if (node->parent == NULL)
+  {
+    return (1);
+  }
+
+  /* Retourne 0 si le nœud a un parent */
+  return (0);
+}
